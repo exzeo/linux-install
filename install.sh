3c9ab76b444f06a9da44c6f09f77d10322abc9c6
Lee Hull <lhull@exzeo.com>
2022-08-30 17:32:40 -0400

    changes

--- install.sh
+++ install.sh
@@ -8,9 +8,11 @@ curl -s -o /tmp/google-chrome-stable_current_amd64.deb https://dl.google.com/lin
 sudo dpkg -i /tmp/google-chrome-stable_current_amd64.deb
 
 # ASDF
-if [ ! -d "~/.asdf" ]; then
-    git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+if [ ! -d "${HOME}/.asdf" ]; then
+    git clone https://github.com/asdf-vm/asdf.git ${HOME}/.asdf
 else
-    git -C ~/.asdf pull
+    git -C ${HOME}/.asdf pull origin master
 fi
-curl -s -o ~/.tool-versions https://raw.githubusercontent.com/exzeo/linux-install/master/files/.tool-versions
+
+# Get tools versions
+curl -s -o ${HOME}/.tool-versions https://raw.githubusercontent.com/exzeo/linux-install/main/files/.tool-versions
