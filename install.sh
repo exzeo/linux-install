d5cd83e29206ea3f3b7232fd67cc8c60c5e5f5ef
Lee Hull <lhull@exzeo.com>
2022-08-30 18:03:08 -0400

    changes

--- install.sh
+++ install.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
 # Dependencies
+echo "Installing dependencies.."
 sudo apt-get install -y git curl
 
 # Chrome
-curl -s -o /tmp/google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i /tmp/google-chrome-stable_current_amd64.deb
+CHROME_PATH=$(which /opt/google/chrome/chrome)
+if [ "${CHROME_PATH}" == "" ]; then
+  echo "Installing chrome..."
+  curl -s -o /tmp/google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  sudo dpkg -i /tmp/google-chrome-stable_current_amd64.deb
+fi
 
 # ASDF
 if [ ! -d "${HOME}/.asdf" ]; then
+    echo "Installing asdf..."
     git clone https://github.com/asdf-vm/asdf.git ${HOME}/.asdf
-else
-    git -C ${HOME}/.asdf pull origin master
 fi
 
 # Get tools versions
 curl -s -o ${HOME}/.tool-versions https://raw.githubusercontent.com/exzeo/linux-install/main/files/.tool-versions
+${HOME}/.asdf/bin/asdf install
